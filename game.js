ae891f3924d96c8aa4128752748fec7946856d7e
Jacki Z <jzamitalo@icloud.com>
2022-08-08 18:26:35 -0400

    Ensure that function is not case sensitive

--- game.js
+++ game.js
@@ -2,21 +2,23 @@ function getComputerChoice() {
     const pick = ['rock', 'paper', 'scissors'];
     return pick[Math.floor(Math.random() * pick.length)];
  }
+
+const computerSelection = getComputerChoice;
  
 function play(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
         return "It's a tie!";
-    } else if (playerSelection == "rock") {
+    } else if (playerSelection.toLowerCase() == "rock") {
         if (computerSelection == "paper") {
             return "You lose! Paper beats rock!";
         } else {
             return "You win! Rock beats scissors!"; }
-    } else if (playerSelection == "paper") {
+    } else if (playerSelection.toLowerCase() == "paper") {
         if (computerSelection == "rock") {
             return "You win! Paper beats rock!";
         } else {
             return "You lose! Scissors beats paper!";}
-    } else if (playerSelection == "scissors") {
+    } else if (playerSelection.toLowerCase() == "scissors") {
         if (computerSelection == "paper") {
             return "You win! Scissors beats paper!"
         } else {
