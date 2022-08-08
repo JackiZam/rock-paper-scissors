0b6a7b2a5885f573f1b67905880a47bf7cbc1753
Jacki Z <jzamitalo@icloud.com>
2022-08-08 18:14:56 -0400

    Create function for computer choice and for play of game

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
-console.log("Hello");
+function getComputerChoice() {
+    const pick = ['rock', 'paper', 'scissors'];
+    return pick[Math.floor(Math.random() * pick.length)];
+ }
+ 
+function play(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            return "You lose! Paper beats rock!";
+        } else {
+            return "You win! Rock beats scissors!"; }
+    } else if (playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            return "You win! Paper beats rock!";
+        } else {
+            return "You lose! Scissors beats paper!";}
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == "paper") {
+            return "You win! Scissors beats paper!"
+        } else {
+            return "You lose! Rock beats scissors!"
+        }
+    }
+
+}
